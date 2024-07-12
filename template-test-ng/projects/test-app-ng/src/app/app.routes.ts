a50a0b47f330d7119332dc76c69744ccfdbe368f
daniel juliao <zigmainflables@gmail.com>
2024-07-12 11:38:10 -0500

    mejoras en front prueba

---
+++ template-test-ng/projects/test-app-ng/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import{TablaComponent} from '../app/tabla/tabla.component';
+
+
+export const routes: Routes = [
+    {path:'', component: TablaComponent},
+    {path:'prueba', component: TablaComponent}
+
+];
